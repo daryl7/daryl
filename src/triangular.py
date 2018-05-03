1548b26a822104c123dc87529a4eda49a92e50a7
daryl <daryl.19690720@gmail.com>
2018-05-03 13:04:57 +0900

    bugfix

--- src/triangular.py
+++ src/triangular.py
@@ -221,12 +221,16 @@ class Triangular:
             applog.info("Total must be at latest %f%s. (min_base_lot = %0.8f)" % (self.__get_lower_limit(base_currency_name, True), base_currency_name, min_base_lot))
             return
 
-        orders[0]["final_lot"] = self.binance.lot_filter(orders[0]["lot"] * min_base_lot / orders[0]["base_lot"], orders[0]["symbol"])
-        if orders[1]["side"] == "BUY":
-            orders[1]["final_lot"] = self.binance.lot_filter(orders[0]["final_lot"] / orders[1]["price"], orders[1]["symbol"], orders[2]["symbol"])
-        elif orders[1]["side"] == "SELL":
-            orders[1]["final_lot"] = self.binance.lot_filter(orders[0]["final_lot"] * orders[1]["price"], orders[1]["symbol"], orders[2]["symbol"])
-        orders[2]["final_lot"] = orders[1]["final_lot"]
+        if orders[1]["side"] == "SELL":
+            # BUY->SELL->SELL
+            orders[0]["final_lot"] = self.binance.lot_filter(orders[0]["lot"] * min_base_lot / orders[0]["base_lot"], orders[0]["symbol"], orders[1]["symbol"]) # target lot
+            orders[1]["final_lot"] = orders[0]["final_lot"]                                                                                                     # target lot
+            orders[2]["final_lot"] = self.binance.lot_filter(orders[1]["final_lot"] * orders[1]["price"],             orders[2]["symbol"])                      # via lot
+        else:
+            # BUY->BUY->SELL
+            orders[0]["final_lot"] = self.binance.lot_filter(orders[0]["lot"] * min_base_lot / orders[0]["base_lot"], orders[0]["symbol"])                      # via lot
+            orders[1]["final_lot"] = self.binance.lot_filter(orders[0]["final_lot"] / orders[1]["price"],             orders[1]["symbol"], orders[2]["symbol"]) # target lot
+            orders[2]["final_lot"] = orders[1]["final_lot"]                                                                                                     # target lot
 
         via_lot = orders[1]["final_lot"] * orders[1]["price"]
         if via_lot < self.__get_lower_limit(via_currency_name, False):

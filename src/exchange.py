90fe76705d8d2849e937e2cf5c79fd6a4c29b9d0
daryl <daryl.19690720@gmail.com>
2018-05-14 23:41:28 +0900

    複数ポジションに対応

--- src/exchange.py
+++ src/exchange.py
@@ -38,80 +38,6 @@ def fetch_url(req, max_times=100, sleep_sec=10):
         time.sleep(sleep_sec)
 
 
-class Context:
-    @staticmethod
-    def get_coin_status():
-        with open('context.yml', 'r') as yml:
-            context = yaml.load(yml)
-        return context['coin_status']
-
-    @staticmethod
-    def set_coin_status(coin_status):
-        with open('context.yml', 'r') as yml:
-            context = yaml.load(yml)
-        context['coin_status'] = coin_status
-        with open('context.yml', 'w') as yml:
-            yml.write(yaml.dump(context, default_flow_style=False))
-
-    @staticmethod
-    def exchange_bitflyer(price, is_to_btc):
-        with open('context.yml', 'r') as yml:
-            context = yaml.load(yml)
-
-        if(is_to_btc):
-            assert not float(context['asset']['bitflyer']['jpy']) == 0.0, "bitflyer jpy is 0"
-            context['asset']['bitflyer']['btc'] = round(float(context['asset']['bitflyer']['jpy']) / float(price), 8)
-            context['asset']['bitflyer']['jpy'] = 0.0
-        else:
-            assert not float(context['asset']['bitflyer']['btc']) == 0.0, "bitflyer btc is 0"
-            context['asset']['bitflyer']['jpy'] = float(context['asset']['bitflyer']['btc']) * price
-            context['asset']['bitflyer']['btc'] = 0.0
-
-        with open('context.yml', 'w') as yml:
-            yml.write(yaml.dump(context, default_flow_style=False))
-
-    @staticmethod
-    def get_bitflyer_btc():
-        with open('context.yml', 'r') as yml:
-            context = yaml.load(yml)
-        return float(context['asset']['bitflyer']['btc'])
-
-    @staticmethod
-    def get_bitflyer_jpy():
-        with open('context.yml', 'r') as yml:
-            context = yaml.load(yml)
-        return float(context['asset']['bitflyer']['jpy'])
-
-    @staticmethod
-    def exchange_binance(price, is_to_btc):
-        with open('context.yml', 'r') as yml:
-            context = yaml.load(yml)
-
-        if(is_to_btc):
-            assert not float(context['asset']['binance']['usd']) == 0.0, "binance usd is 0"
-            context['asset']['binance']['btc'] = round(float(context['asset']['binance']['usd']) / float(price), 6)
-            context['asset']['binance']['usd'] = 0.0
-        else:
-            assert not float(context['asset']['binance']['btc']) == 0.0, "binance btc is 0"
-            context['asset']['binance']['usd'] = float(context['asset']['binance']['btc']) * price
-            context['asset']['binance']['btc'] = 0.0
-
-        with open('context.yml', 'w') as yml:
-            yml.write(yaml.dump(context, default_flow_style=False))
-
-    @staticmethod
-    def get_binance_btc():
-        with open('context.yml', 'r') as yml:
-            context = yaml.load(yml)
-        return float(context['asset']['binance']['btc'])
-
-    @staticmethod
-    def get_binance_usd():
-        with open('context.yml', 'r') as yml:
-            context = yaml.load(yml)
-        return float(context['asset']['binance']['usd'])
-
-
 class Exchange:
     def __init__(self):
         self.ask = 0
@@ -211,9 +137,9 @@ class BitFlyer(Exchange):
                     break
         return [child_order_acceptance_id, commission]
 
-    def buy_order_from_available_balance(self, dryrun):
+    def buy_order_from_available_balance(self, jpy, dryrun):
         price = self.ask + config['trader']['order_offset_jpy']
-        lot = round(Context.get_bitflyer_jpy() / float(price), 8)
+        lot = round(jpy / float(price), 8)
         if not dryrun:
             body = {
                 "product_code": "BTC_JPY",
@@ -228,15 +154,13 @@ class BitFlyer(Exchange):
         else:
             child_order_acceptance_id = "demo"
             commission = round(lot * 0.0015,8)
-        Context.exchange_bitflyer(price, True)
         self.last_buy_price = price
         self.last_buy_lot = lot
         self.last_buy_commission = commission
         return "\tbuy_order:BitFlyer, price:" + str(price) + ", lot:" + str(lot) + ", commission:" + str(commission) + ", child_order_acceptance_id:" + child_order_acceptance_id
 
-    def sell_order_from_available_balance(self, dryrun):
+    def sell_order_from_available_balance(self, lot, dryrun):
         price = self.bid - config['trader']['order_offset_jpy']
-        lot = Context.get_bitflyer_btc()
         if not dryrun:
             body = {
                 "product_code": "BTC_JPY",
@@ -251,7 +175,6 @@ class BitFlyer(Exchange):
         else:
             child_order_acceptance_id = "demo"
             commission = round(lot * 0.0015,8)
-        Context.exchange_bitflyer(price, False)
         self.last_sell_price = price
         self.last_sell_lot = lot
         self.last_sell_commission = commission
@@ -348,9 +271,9 @@ class Binance(Exchange):
         # TODO: implement
         return True
 
-    def buy_order_from_available_balance(self, dryrun):
+    def buy_order_from_available_balance(self, usd, dryrun):
         price = self.ask + config['trader']['order_offset_usd']
-        lot = round(Context.get_binance_usd() / float(price), 6)
+        lot = round(usd / float(price), 6)
         if(not dryrun):
             order = self.client.create_order(
                 symbol = 'BTCUSDT',
@@ -359,15 +282,13 @@ class Binance(Exchange):
                 timeInForce = binance.client.Client.TIME_IN_FORCE_GTC,
                 quantity = lot,
                 price = price )
-        Context.exchange_binance(price, True)
         self.last_buy_price = price
         self.last_buy_lot = lot
         self.last_buy_comission = round(lot * self.comission_fee, 8)
         return "\tbuy_order: Binance, " + str(price) + ", " + str(lot)
 
-    def sell_order_from_available_balance(self, dryrun):
+    def sell_order_from_available_balance(self, lot, dryrun):
         price = self.bid - config['trader']['order_offset_usd']
-        lot = Context.get_binance_btc()
         if(not dryrun):
             order = self.client.create_order(
                 symbol = 'BTCUSDT',
@@ -376,7 +297,6 @@ class Binance(Exchange):
                 timeInForce = binance.client.Client.TIME_IN_FORCE_GTC,
                 quantity = lot,
                 price = price )
-        Context.exchange_binance(price, False)
         self.last_sell_price = price
         self.last_sell_lot = lot
         self.last_sell_comission = round(lot * self.comission_fee, 8)
--- src/trader.py
+++ src/trader.py
@@ -5,7 +5,7 @@ import yaml
 import monitor
 from enum import IntEnum, auto
 from monitor import Monitor
-from exchange import BitFlyer, Binance, Context
+from exchange import BitFlyer, Binance
 import smtplib
 from email.mime.text import MIMEText
 import sys
@@ -15,33 +15,32 @@ import traceback
 from mailer import Mailer
 
 
-class CoinStatus(IntEnum):
-    BitFlyer = auto()
-    CoinCheck = auto()
-    Binance = auto()
+class CoinStatus:
+    BitFlyer = "BitFlyer"
+    Binance = "Binance"
+
+
+class LogFiles:
+    batch_output_filename = './trade_simulation.tsv'
+    trade_log_full_filepath = './log/trade_full.log'
+    trade_tsv_filepath = './log/trade.tsv'
 
 
 class Trader:
     def __init__(self, *args, **kwargs):
         with open('config.yml', 'r') as yml:
             config = yaml.load(yml)
-            self.bf_bn_limit = config['trader']['bf_bn_limit']
-            self.bn_bf_limit = config['trader']['bn_bf_limit']
             self.order_offset_jpy = config['trader']['order_offset_jpy']
             self.order_offset_usd = config['trader']['order_offset_usd']
-            self.notification_email_to = config['notifycation']['email']['to']
-            self.notification_email_from = config['notifycation']['email']['from']
-            self.notification_email_subject = config['notifycation']['email']['subject']
-        self.batch_output_filename = './trade_simulation.tsv'
-        self.trade_log_full_filepath = './log/trade_full.log'
-        self.trade_tsv_filepath = './log/trade.tsv'
+        self.load_positions()
 
     def trade(self, run_mode):
-        if os.path.exists(self.batch_output_filename):
-            os.remove(self.batch_output_filename)
+        if os.path.exists(LogFiles.batch_output_filename):
+            os.remove(LogFiles.batch_output_filename)
 
-        if self.is_email_notification():
-            if not self.checkmailer():
+        mailer = Mailer()
+        if mailer.is_use():
+            if not mailer.checkmailer():
                 applog.error("mailer not activation!")
                 sys.exit()
 
@@ -50,19 +49,16 @@ class Trader:
         else:
             dryrun = True
 
-        coin_status = CoinStatus.BitFlyer if Context.get_coin_status() == "BitFlyer" else CoinStatus.Binance
         mon = Monitor()
 
         applog.info("========================================")
         applog.info("Start Trader. RunMode = " + run_mode)
         applog.info("binance.comission_fee: " + str(mon.binance.comission_fee))
-        applog.info("bf_bn_limit = " + str(self.bf_bn_limit))
-        applog.info("bn_bf_limit = " + str(self.bn_bf_limit))
         applog.info("order_offset_jpy = " + str(self.order_offset_jpy))
         applog.info("order_offset_usd = " + str(self.order_offset_usd))
-        applog.info("notification_email_to = " + self.notification_email_to)
-        applog.info("notification_email_from = " + self.notification_email_from)
-        applog.info("notification_email_subject = " + self.notification_email_subject)
+        applog.info("notification_email_to = " + mailer.notification_email_to)
+        applog.info("notification_email_from = " + mailer.notification_email_from)
+        applog.info("notification_email_subject = " + mailer.notification_email_subject)
         applog.info("========================================")
 
         if run_mode == "Batch":
@@ -76,25 +72,54 @@ class Trader:
                 mon.binance.bid = float(row[5])
                 mon.binance.ask = float(row[6])
                 mon.usdjpy = float(row[7])
-                coin_status = self.decision_and_order(coin_status, mon, dryrun)
+                for position in self.positions:
+                    position.decision_and_order(mon, dryrun)
         else:
             try:
                 while True:
                     mon.refresh()
                     if mon.validation_check():
-                        coin_status = self.decision_and_order(coin_status, mon, dryrun)
+                        execution = False
+                        for position in self.positions:
+                            if position.decision_and_order(mon, dryrun):
+                                execution = True
+                        if execution:
+                            self.save_positions()
                     time.sleep(3)
             except Exception as e:
                 applog.error(traceback.format_exc())
-                Mailer().sendmail(traceback.format_exc())
+                mailer.sendmail(traceback.format_exc())
 
-    def decision_and_order(self, coin_status, mon, dryrun):
+    def load_positions(self):
+        self.positions = []
+        with open('positions.yml', 'r') as yml:
+            yml_positions = yaml.load(yml)
+        for hash in yml_positions:
+            self.positions.append(Position(hash))
+
+    def save_positions(self):
+        yml_positions = []
+        for position in self.positions:
+            yml_positions.append(position.hash)
+        with open('positions.yml', 'w') as yml:
+            yml.write(yaml.dump(yml_positions, default_flow_style=False))
+
+
+class Position:
+    def __init__(self, hash):
+        self.hash = hash
+
+    def decision_and_order(self, mon, dryrun):
+        execution = False
         row = [mon.dt, str(mon.bf_bn_diff), str(mon.bn_bf_diff), str(mon.bitflyer.bid), str(mon.bitflyer.ask), str(mon.binance.bid), str(mon.binance.ask), str(mon.usdjpy)]
-        if coin_status == CoinStatus.BitFlyer and mon.bf_bn_diff >= self.bf_bn_limit:
+        if self.hash["coin_status"] == CoinStatus.BitFlyer and mon.bf_bn_diff >= self.hash["bf_bn_limit"]:
             if mon.health_check(dryrun):
-                coin_status = CoinStatus.Binance
-                self.order(mon, mon.binance, mon.bitflyer, mon.bf_bn_diff, dryrun)
+                execution = True
+                message1 = "BitFlyer->Binance(" + mon.dt + ", diff:" + str(mon.bf_bn_diff) + ")"
+                message2 = mon.bitflyer.sell_order_from_available_balance(self.hash["asset"]["bitflyer"]["btc"], dryrun)
+                message3 = mon.binance.buy_order_from_available_balance(self.hash["asset"]["binance"]["usd"], dryrun)
                 row.extend([
+                    self.hash["_label"],
                     "SELL",
                     str(mon.bitflyer.last_sell_price),
                     str(mon.bitflyer.last_sell_lot),
@@ -104,12 +129,18 @@ class Trader:
                     str(mon.binance.last_buy_lot),
                     str(mon.binance.last_buy_comission)
                 ])
+                self.exchange_bitflyer(mon.bitflyer.last_sell_price, False)
+                self.exchange_binance(mon.binance.last_buy_price, True)
+                self.hash["coin_status"] = CoinStatus.Binance
                 self.trade_log(row)
-        elif coin_status == CoinStatus.Binance and mon.bn_bf_diff >= self.bn_bf_limit:
+        elif self.hash["coin_status"] == CoinStatus.Binance and mon.bn_bf_diff >= self.hash["bn_bf_limit"]:
             if mon.health_check(dryrun):
-                coin_status = CoinStatus.BitFlyer
-                self.order(mon, mon.bitflyer, mon.binance, mon.bn_bf_diff, dryrun)
+                execution = True
+                message1 = "Binance->BitFlyer(" + mon.dt + ", diff:" + str(mon.bn_bf_diff) + ")"
+                message2 = mon.bitflyer.buy_order_from_available_balance(self.hash["asset"]["bitflyer"]["jpy"], dryrun)
+                message3 = mon.binance.sell_order_from_available_balance(self.hash["asset"]["binance"]["btc"], dryrun)
                 row.extend([
+                    self.hash["_label"],
                     "BUY",
                     str(mon.bitflyer.last_buy_price),
                     str(mon.bitflyer.last_buy_lot),
@@ -119,57 +150,56 @@ class Trader:
                     str(mon.binance.last_sell_lot),
                     str(mon.binance.last_sell_comission)
                 ])
+                self.exchange_bitflyer(mon.bitflyer.last_buy_price, True)
+                self.exchange_binance(mon.binance.last_sell_price, False)
+                self.hash["coin_status"] = CoinStatus.BitFlyer
                 self.trade_log(row)
-        return coin_status
 
-    def order(self, monitor, buying_exchange, selling_exchange, diff, dryrun):
-        message1 = selling_exchange.__class__.__name__ + "->" + buying_exchange.__class__.__name__ + "(" + monitor.dt + ", diff:" + str(diff) + ")"
-        message2 = buying_exchange.buy_order_from_available_balance(dryrun)
-        message3 = selling_exchange.sell_order_from_available_balance(dryrun)
-        applog.info(message1)
-        applog.info(message2)
-        applog.info(message3)
-        with open(self.trade_log_full_filepath, mode = 'a', encoding = 'utf-8') as fh:
-            fh.write('\n'.join(["", message1, message2, message3, ""]))
-        Context.set_coin_status(buying_exchange.__class__.__name__)
+        if execution:
+            applog.info(message1)
+            applog.info(message2)
+            applog.info(message3)
+            with open(LogFiles.trade_log_full_filepath, mode = 'a', encoding = 'utf-8') as fh:
+                fh.write('\n'.join(["", message1, message2, message3, ""]))
 
-        if self.is_email_notification():
-            self.sendmail(message1 + "\n" + message2 + "\n" + message3)
+            mailer = Mailer()
+            if mailer.is_use():
+                mailer.sendmail(message1 + "\n" + message2 + "\n" + message3)
+
+        return execution
 
     def trade_log(self, row):
         record = "\t".join(row)
         files = [
-            self.batch_output_filename,
-            self.trade_log_full_filepath,
-            self.trade_tsv_filepath
+            LogFiles.batch_output_filename,
+            LogFiles.trade_log_full_filepath,
+            LogFiles.trade_tsv_filepath
         ]
         for file in files:
             with open(file, mode = 'a', encoding = 'utf-8') as fh:
                 fh.write(record + '\n')
 
-    def is_email_notification(self):
-        return not self.notification_email_to == ""
+    def exchange_bitflyer(self, price, is_to_btc):
+        asset = self.hash["asset"]
+        if(is_to_btc):
+            assert not float(asset['bitflyer']['jpy']) == 0.0, "bitflyer jpy is 0"
+            asset['bitflyer']['btc'] = round(float(asset['bitflyer']['jpy']) / float(price), 8)
+            asset['bitflyer']['jpy'] = 0.0
+        else:
+            assert not float(asset['bitflyer']['btc']) == 0.0, "bitflyer btc is 0"
+            asset['bitflyer']['jpy'] = float(asset['bitflyer']['btc']) * price
+            asset['bitflyer']['btc'] = 0.0
 
-    def sendmail(self, message):
-        you = self.notification_email_to
-        me = self.notification_email_from
-        msg = MIMEText(message)
-        msg['Subject'] = self.notification_email_subject
-        msg['To'] = you
-        msg['From'] = me
-        s = smtplib.SMTP()
-        s.connect()
-        s.sendmail(me, [you], msg.as_string())
-        s.close()
-
-    def checkmailer(self):
-        s = smtplib.SMTP()
-        try:
-            s.connect()
-            s.close()
-        except ConnectionRefusedError:
-            return False
-        return True
+    def exchange_binance(self, price, is_to_btc):
+        asset = self.hash["asset"]
+        if(is_to_btc):
+            assert not float(asset['binance']['usd']) == 0.0, "binance usd is 0"
+            asset['binance']['btc'] = round(float(asset['binance']['usd']) / float(price), 6)
+            asset['binance']['usd'] = 0.0
+        else:
+            assert not float(asset['binance']['btc']) == 0.0, "binance btc is 0"
+            asset['binance']['usd'] = float(asset['binance']['btc']) * price
+            asset['binance']['btc'] = 0.0
 
 
 if __name__ == '__main__':
